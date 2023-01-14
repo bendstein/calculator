56b0aa65ffcfce4c2596896d64f040f305ba3a3e
bendstein <bendstein@gmail.com>
2023-01-13 23:54:33 -0500

    Allow window resize

--- desktop_app/src/main.rs
+++ desktop_app/src/main.rs
@@ -12,7 +12,7 @@ fn main() -> Result<(), tauri::Error> {
   let builder = tauri::Builder::default()
     .setup(|app| {
       let main_window = app.get_window("main").unwrap();
-      main_window.set_resizable(false)?;
+      //main_window.set_resizable(false)?;
       main_window.set_size(LogicalSize::new(WIDTH, HEIGHT))?;
       Ok(())
     });
